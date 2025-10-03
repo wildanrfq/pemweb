d9fe559117611602b9c30ef49208976941ea3629
will <33510837+wildanrfq@users.noreply.github.com>
2025-10-03 19:06:12 +0700

    Add files via upload

---
+++ latkuis/script.js
@@ -0,0 +1,13 @@
+function navigateLeft() {
+    window.scrollBy({
+        left: 300,
+        behavior: 'smooth',
+    })
+}
+
+function navigateRight() {
+    window.scrollBy({
+        left: -300,
+        behavior: 'smooth',
+    })
+}
